bdd70b8f494ccee31faba072a8fac4a14a78663c
zhouzishu <me@zzs.im>
2021-01-09 10:34:36 +0800

    更改“更新时间”的样式

--- docs/js/plugins/auto-add-update-time.js
+++ docs/js/plugins/auto-add-update-time.js
@@ -4,7 +4,7 @@ function auto_add_update_time_plugin(hook, vm) {
 
   hook.beforeEach(function(content) {
     vm.config.autoHeader = false;
-    content = "<p align=\"right\">更新时间：{docsify-updated}</p>\n\n"+content
+    content = "<p align=\"right\" style=\"opacity: .6;\">更新时间：{docsify-updated}</p>\n\n"+content
     return content;
   });
 }
